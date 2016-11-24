2fdeeda9c20ef80698736b32edd79fd6a29a5627
Parsh Jain <parsh.cosmos11@gmail.com>
2016-11-24 19:34:52 +0530

    AngularBased

---
+++ src/common/common.module.js
@@ -0,0 +1,14 @@
+(function() {
+    "use strict";
+
+    angular.module('common', [])
+        .constant('ApiPath', 'https://ruby-server.herokuapp.com')
+        .config(config);
+
+    config.$inject = ['$httpProvider'];
+
+    function config($httpProvider) {
+        $httpProvider.interceptors.push('loadingHttpInterceptor');
+    }
+
+})();
